916ed9dbb283050452d0387209affb9a64aa0dcf
Alan-Melnychenko <93719770+Alan-Melnychenko@users.noreply.github.com>
2021-12-28 13:41:29 +0200

    Lab

---
+++ LR4/4(4).py
@@ -0,0 +1,11 @@
+﻿import math
+x = float(input('Введіть перше число: '))
+y = float(input('Введіть друге число: '))
+z = 0
+if y < x:
+    z = y * math.e ** x
+elif y > x:
+    z = x * math.e ** y
+else:
+    z = y * x
+print('Число z={0:.2f}'.format(z))
